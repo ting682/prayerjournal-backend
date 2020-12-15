49cf693abec7ecacf306588b0c329b69ce5a7c8a
ting682 <tchung682@gmail.com>
2020-12-15 13:58:11 -0500

    changed cors for production build

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    # origins 'http://localhost:3001'
+    origins 'https://prayerjournal.netlify.app'
 
     resource '*',
       headers: :any,
