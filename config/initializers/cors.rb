95ba100318cd7ac89c218659ef67a04c3cae56db
ting682 <tchung682@gmail.com>
2020-12-17 14:06:36 -0500

    cors added

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'https://prayerjournal.netlify.app'
+    origins 'http://localhost:3001', 'https://prayerjournal.netlify.app', 'https://prayerjournal.place'
     # origins 'https://prayerjournal.netlify.app'
 
     resource '*',
