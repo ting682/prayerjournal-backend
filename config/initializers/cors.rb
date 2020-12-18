b16d6de8c15505a0337579fbe7f7a35307aedf64
ting682 <tchung682@gmail.com>
2020-12-18 09:19:48 -0500

    changed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'https://prayerjournal.place'
-    origins 'http://localhost:3001' 
+    origins 'https://prayerjournal.place'
+    # origins 'http://localhost:3001' 
 
     resource '*',
       headers: :any,
