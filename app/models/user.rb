3c59bc4b26ac9c2465713199f464410ba91a0ca3
ting682 <tchung682@gmail.com>
2020-12-28 17:32:26 -0500

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
     has_many :entries
     has_secure_password
     validates :email_address, presence: true, uniqueness: true
-    
+    validates :name, presence: true
+    validates :bio, presence: true
 end
