19bfdaa2d26ab69eb0a944073087dbf29fb1d980
ting682 <tchung682@gmail.com>
2021-01-05 20:19:36 -0500

    changed blogentry

--- app/models/canceled/blog_entry.rb
+++
@@ -1,7 +0,0 @@
-class BlogEntry < ApplicationRecord
-    # belongs_to :user
-    # has_many :likes
-    # has_many :comments
-    # validates :content, presence: true
-    # belongs_to :blog, optional: true
-end
