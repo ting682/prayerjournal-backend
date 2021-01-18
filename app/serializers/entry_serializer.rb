6f7444a517a7ecb479f946efdc9105843005838b
ting682 <tchung682@gmail.com>
2021-01-18 07:34:56 -0500

    added to entry serializer

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,6 +1,6 @@
 class EntrySerializer
   include FastJsonapi::ObjectSerializer
-  attributes :content, :name, :likes_count, :created_at, :updated_at, :user_id, :public, :image_url, :text_content, :blog_id, :video_url
+  attributes :content, :name, :likes_count, :created_at, :updated_at, :user_id, :public, :image_url, :text_content, :blog_id, :video_url, :published
   has_many :comments
   has_many :likes
   has_many :hashtags
